095387a8c52aa85cfd3e1e03ef565af1aa0922a5
shlyambur <shlyambur@gmail.com>
2016-05-17 15:46:35 +0300

    Update test3.sh

--- test3.sh
+++ test3.sh
@@ -1,4 +1,2 @@
-sudo wget -O ~/project/chef_12.9.41-1_i386.deb https://packages.chef.io/stable/ubuntu/10.04/chef_12.9.41-1_i386.deb
 sudo dpkg -i ~/project/chef/chef_12.9.41-1_i386.deb
-
 sudo chef-solo -c ~/project/solo.rb -j ~/project/solo.json
