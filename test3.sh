2a1485d2a6376e2677c965497578735787cefb60
shlyambur <shlyambur@gmail.com>
2016-05-19 16:56:15 +0300

    Update test3.sh

--- test3.sh
+++ test3.sh
@@ -1,2 +1,10 @@
-sudo dpkg -i ~/project/chef_12.9.41-1_i386.deb
+if [ "$(getconf LONG_BIT)" = 32 ];
+then
+        sudo wget -O ~/project/chef_12.9.41-1_i386.deb https://packages.chef.io/stable/ubuntu/10.04/chef_12.9.41-1_i386.deb
+        sudo dpkg -i ~/project/chef_12.9.41-1_i386.deb
+else
+        sudo wget -O ~/project/chef_12.9.41-1_amd64.deb https://packages.chef.io/stable/ubuntu/10.04/chef_12.9.41-1_amd64.deb
+        sudo dpkg -i ~/project/chef_12.9.41-1_amd64.deb
+fi
+
 sudo chef-solo -c ~/project/chef/solo.rb -j ~/project/chef/solo.json
